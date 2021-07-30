03d04cb78468085c7263bbe19bf3c650aca49605
ben thacker <benthac@gmail.com>
2021-07-30 19:35:47 -0400

    updating .js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const cityFormEl = document.querySelector("form");
+const cityInputEl = document.querySelector("")
+
+
+
 const getWeather = function(city) {
 
     const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=54654b2970f4ff24c637b2ecba8f2f9f" 
