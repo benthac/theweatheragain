1c608630b1618fa6d7dff09b41c4605561f0b952
ben thacker <benthac@gmail.com>
2021-07-31 22:30:15 -0400

    for loop, vars, functions

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-const cityFormEl = document.querySelector("form");
-const cityInputEl = document.querySelector("")
+const weatherContainerEl = document.querySelector("#weather-container");
+const weatherSearchEl = document.querySelector("#city-search-name");
+const cityFormEl = document.querySelector("#city-form");
+const cityInputEl = document.querySelector("#city");
 
+const formSubmitHandler = function(event) {
+    event.preventDefault();
+    let cityname = cityInputEl.value.trim();
 
+    if (cityname) {
+        getWeather(cityname);
+        cityInputEl.value = '';
+    } else {
+        alert("You need to enter a city");
+    }
+};
+
+const showCityWeather = function(weather, searchTerm) {
+    weatherContainerEl.textContent = "";
+    weatherSearchEl.textContent = searchTerm;
+    for (var i = 0; i < weather.length; i++) {
+        var cityName = weather[i].city.id + "/" + weather[i].name;
+        var weather = document.createElement("div");
+        weather.classList = "list-item flex-row justify-space-between align-center";
+        var weatherEl = document.createElement("span");
+        weatherEl.textContent = cityName;
+        weather.appendChild(weatherEl);
+        weatherContainerEl.appendChild(weather); 
+    }
+};
 
 const getWeather = function(city) {
 
@@ -9,9 +35,9 @@ const getWeather = function(city) {
 
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            showCityWeather(data, city);
         });
     });
 };
 
-getWeather("tulsa");
+cityFormEl.addEventListener("submit", formSubmitHandler);
